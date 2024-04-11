d24872cce873b6f9568a8e52196b1bcb1796a422
Chaitanya Jha <138287343+wafflebytes@users.noreply.github.com>
2024-04-11 12:16:52 +0000

    Changed a little bit

--- uninstall/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#remove cmdtypist and associated files.
-sudo rm /usr/bin/cmdtypist &&
-sudo rm -r $HOME/cmdTypist &&
-echo "cmdtypist uninstalled" ||
-echo "unable to uninstall cmdtypist"; exit $?
