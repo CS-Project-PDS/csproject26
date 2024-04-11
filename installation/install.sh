7a085c1fd915a7c3d495e9abc423fb72aeb666b4
Chaitanya Jha <138287343+wafflebytes@users.noreply.github.com>
2024-04-11 12:19:29 +0000

    Changed

--- installation/install.sh
+++ installation/install.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-#install cmdtypist 
 gcc ../cmd_typist.c -o ../cmdtypist &&
 sudo cp ../cmdtypist /usr/bin/cmdtypist &&
 mkdir $HOME/cmdTypist && 
@@ -7,17 +5,4 @@ cp -r ../ $HOME/cmdTypist/ &&
 sudo cp ../cmdtypist /usr/bin/cmdtypist &&
 sudo chmod +x /usr/bin/cmdtypist &&
 (echo "CMDTYPIST successfully compiled and installed, learn how to use cmdtypist commands by running 'cmdtypist --help'"; exit 0) ||
-(c=$?; echo "CMDTYPIST was unable to install: "; (exit $c)) 
-
-
-# if [echo "Enter root password: "]; then 
-# 	if [sudo gcc ./cmdtypist.c -o ./cmdtypist]; then
-# 		if [[ sudo mkdir /opt/cmdtypist ]]; then
-# 		 	if [[ sudo cp -r . /opt/cmtypist/ ]]; then
-# 		 		echo "Cmdtypist successfully compiled and installed, learn how to use cmdtypist commands by running 'cmdtypist --help'"
-# 		 	else
-# 		 		echo "cmdtypist was unable to install, make sure the directory /opt exist and try again"
-# 		 	fi
-# 		fi 
-# 	fi
-# fi
+(c=$?; echo "CMDTYPIST was unable to install: "; (exit $c))
